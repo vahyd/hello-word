90fb4cab38458030c283d67da4ada82c52ef6986
vahyd <vahyd.khatami@gmail.com>
2017-08-05 01:02:51 -0400

    Update mount.test.js

--- my-app/src/mount.test.js
+++ my-app/src/mount.test.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import sinon from 'sinon';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import App from './App';
 
-describe('<App/>', () => {
+describe('<App />', () => {
   it('calls componentDidMount', () => {
     sinon.spy(App.prototype, 'componentDidMount');
     const wrapper = mount(<App />);
     expect(App.prototype.componentDidMount).to.have.property('callCount', 1);
     App.prototype.componentDidMount.restore();
   });
+
+});
+describe('<App />', () => {
+      it('Returns json file', (done) => {
+        const wrapper = mount(<App />);
+          setTimeout(() => {
+            expect(wrapper.state().details).to.not.have.length(0);
+            done();
+         }, 3000);
+       });
 });
